23285cdd29f55eefc8466cf47c226cafc27516ec
likai <1195419506@qq.com>
2024-10-09 23:16:20 +0800

    feat: 添加开发环境地址

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,4 +1,10 @@
 export default {
   lkBaseURL: 'https://www.kaisir.cn',
   lkChatBaseURL: 'https://www.kaisir.cn/chat-api',
+  // dev
+  // lkBaseURL: 'http://192.168.6.131:8181',
+  // lkChatBaseURL: 'http://192.168.6.131:8181/chat-api',
+  // local
+  // lkBaseURL: 'http://127.0.0.1:8080',
+  // lkChatBaseURL: 'http://127.0.0.1:8080/chat-api',
 }
